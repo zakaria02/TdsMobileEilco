e0d4c18fdc0e82e3da39ce0f73195c35a301c7a3
Zakaria Bensalem <zakaria.bensalem@conacom.net>
2021-12-13 18:55:33 +0100

    TD5

--- TD5/app/src/main/java/com/example/td5/MainActivity.kt
+++ TD5/app/src/main/java/com/example/td5/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         contacts.add(Contact("KORIS", "Abdellah","https://media-exp1.licdn.com/dms/image/C4D03AQEhR0F0coq0QA/profile-displayphoto-shrink_200_200/0/1618001218426?e=1643846400&v=beta&t=gjk1wmJbWCAuBGk8JqV3cC0Gz0o34cALEuvxDzTrA8Y"))
         contacts.add(Contact("ENNOURI", "Hind","https://media-exp1.licdn.com/dms/image/C4E03AQHcLWAOFknYpw/profile-displayphoto-shrink_100_100/0/1636048518047?e=1642636800&v=beta&t=7PwUwUNTGcn-XBXGQnAZhrwCa6YMBd7Pt-oXUspTpHU"))
 
+
         var ca:ContactAdapter = ContactAdapter(contacts)
 
         rv.adapter = ca
